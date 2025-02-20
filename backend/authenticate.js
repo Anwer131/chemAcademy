25e29615de2c5f83e0549f04d8428f55f23b39c8
Munazzil Anwer <munazziliitd@gmail.com>
2025-02-21 01:18:06 +0530

    fix: updated authentication logic and improved error handling

--- backend/authenticate.js
+++ backend/authenticate.js
@@ -1,58 +1,54 @@
-var passport = require('passport');
-var User = require('./models/user');
-var LocalStrategy = require('passport-local').Strategy;
-var JwtStrategy = require('passport-jwt').Strategy;
-var ExtractJwt = require('passport-jwt').ExtractJwt;
-var jwt = require('jsonwebtoken');
+const passport = require('passport');
+const User = require('./models/user');
+const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken');
+const config = require('./config');
 
-var config = require('./config');
+// Local Strategy
+passport.use(new LocalStrategy(
+  { usernameField: 'username', passwordField: 'password' },
+  User.authenticate()
+));
 
-passport.use(new LocalStrategy(User.authenticate()));
+// Serialize and deserialize user
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = (user) => { // creates json web token
-    return jwt.sign(user, config.secretKey, {
-        expiresIn: '8h' // token will expire in 360000 seconds
-    }) 
+// Generate JWT Token
+exports.getToken = (user) => {
+  return jwt.sign({ _id: user._id }, config.secretKey, { expiresIn: '8h' });
 };
 
-var opts = {}; // Options for jwt strategy
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); // This will extract the token that will be later used to verify the user.
-opts.secretOrKey = config.secretKey;
+// JWT Strategy Options
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: config.secretKey,
+};
 
-exports.jwtPassport = passport.use(new JwtStrategy(opts,
-    (jwt_payload,done) => {
-        // console.log("JWT payload :",jwt_payload);
-        User.findOne({ _id: jwt_payload._id })
-        .then(user => {
-            if (user) {
-                // If user is found, return user
-                return done(null, user);
-            } else {
-                // If user is not found, return false
-                return done(null, false);
-            }
-        })
-        .catch(err => {
-            // If an error occurs, return the error
-            return done(err, false);
-        });
+// JWT Strategy
+passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+  console.log("🔍 JWT payload:", jwt_payload);  // Debug log for payload
+  User.findById(jwt_payload._id)
+    .then(user => {
+      if (user) return done(null, user);
+      return done(null, false);
+    })
+    .catch(err => done(err, false));
+}));
 
-}))
+// Middleware to verify JWT
+exports.verifyUser = passport.authenticate('jwt', { session: false });
 
-exports.verifyUser = passport.authenticate('jwt',{session: false}) // This implies that we are not going to create any session unlike previous case.
+// Middleware to verify admin privileges
 exports.verifyAdmin = (req, res, next) => {
-    User.findById({_id: req.user._id})
-    .then((user) => {
-        if (user.admin) {
-            next();
-        }
-        else {
-            err = new Error('You are not authorized to perform this operation!');
-            err.status = 403;
-            return next(err);
-        }
-    },(err) => next(err))
-    .catch((err) => next(err))
-}
+  User.findById(req.user._id)
+    .then(user => {
+      if (user && user.admin) return next();
+      const err = new Error('❌ You are not authorized to perform this operation!');
+      err.status = 403;
+      return next(err);
+    })
+    .catch(err => next(err));
+};
