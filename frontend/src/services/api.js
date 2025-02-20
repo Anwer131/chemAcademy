4b2fb49e45180ff9332ddcda14ef262fdfcb23d7
Munazzil Anwer <munazziliitd@gmail.com>
2025-02-21 01:37:09 +0530

    api url updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = "https://chemixlib-api.up.railway.app";
 
 /* Get token from localStorage*/
 const getToken = () => localStorage.getItem('token');
